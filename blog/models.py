41259640b85c107df8430ce05161b2d44b66e4b2
Kirill Leontev <kyrary99@gmail.com>
2019-01-10 13:45:22 +0200

    Add posts on main page

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
 
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    posts = Post.objects.order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})
